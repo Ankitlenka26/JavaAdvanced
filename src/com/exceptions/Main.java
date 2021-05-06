4cd03092f3a93299b96f77184ed1d9b3947eaadf
Ankit Lenka <ankitlenka26@gmail.com>
2021-05-06 15:04:18 +0530

    generics intro

--- src/com/exceptions/Main.java
+++ src/com/exceptions/Main.java
@@ -1,16 +1,16 @@
 package com.exceptions;
 
 import Exceptions.ExceptionsDemo;
+import Generics.List;
 
 import java.io.IOException;
 
 public class Main {
 
     public static void main(String[] args) {
-        try {
-            ExceptionsDemo.show();
-        } catch (Throwable e) {
-            System.out.println("An unexpected error Occured!!");
-        }
+        var list = new List();
+        list.add(1);
+        // WHAT IF WE WANT TO MAKE A LIST OF USERS(WHICH ARE OBJECTS) THEN WE CANNOT USE THE LIST CLASS THERE AND HAVE TO FORM NEW CLASS
+        // WHICH IS JUST CODE REPETITION AND NOT VERY SCALABLE
     }
 }
