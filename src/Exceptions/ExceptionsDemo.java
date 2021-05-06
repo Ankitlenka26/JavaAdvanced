c9c6afe301f07fd8011bf65aefa778d7d60a8ad8
Ankit Lenka <ankitlenka26@gmail.com>
2021-05-06 12:55:12 +0530

    try with resource statement

--- src/Exceptions/ExceptionsDemo.java
+++ src/Exceptions/ExceptionsDemo.java
@@ -1,31 +1,20 @@
 package Exceptions;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
 public class ExceptionsDemo {
     public static void show(){
-        // if we can catch IOException we do not need to write FileNotFound exception as they are internally related
-        // the calling of file not found exception after an io exception is redundant . the order of catching methods also matter in some cases
-        FileReader reader = null;
-        try{
-            reader = new FileReader("file.txt");
+        // this class should implement the AutoClosable interface for this to work
+        try(var reader = new FileReader("file.txt");
+        var writer = new FileWriter("...")
+        ){ // Now we don't need to explicitly close the resource inside the finally
+            // block , the java compiler does it for us
             var value = reader.read();
         }catch (IOException e){
             // if we cannot read
-            System.out.println("Could not read data+");
-        }finally { // it will always get get executed
-            if(reader != null) {
-                try {
-                    reader.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
+            System.out.println("Could not read data!");
         }
     }
     public static void sayHello(String name){
