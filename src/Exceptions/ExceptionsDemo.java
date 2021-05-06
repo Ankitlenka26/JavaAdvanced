ad24141d1946a2b2a65eef1c24f364242779a54c
Ankit Lenka <ankitlenka26@gmail.com>
2021-05-06 12:40:51 +0530

    Finally Block

--- src/Exceptions/ExceptionsDemo.java
+++ src/Exceptions/ExceptionsDemo.java
@@ -1,5 +1,6 @@
 package Exceptions;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -10,21 +11,22 @@ public class ExceptionsDemo {
     public static void show(){
         // if we can catch IOException we do not need to write FileNotFound exception as they are internally related
         // the calling of file not found exception after an io exception is redundant . the order of catching methods also matter in some cases
+        FileReader reader = null;
         try{
-            new SimpleDateFormat().parse("");
-            var reader = new FileReader("file.txt");
+            reader = new FileReader("file.txt");
             var value = reader.read();
-            System.out.println("File opened");
         }catch (IOException e){
             // if we cannot read
-            System.out.println("Could not read file");
-        }catch (ParseException e){
-            System.out.println(e.getMessage());
+            System.out.println("Could not read data+");
+        }finally { // it will always get get executed
+            if(reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
-//        catch(FileNotFoundException ex){
-//            // if file is not found
-//            System.out.println("File does not exist");
-//        }
     }
     public static void sayHello(String name){
         System.out.println(name.toUpperCase());
