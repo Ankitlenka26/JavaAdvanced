863316704475270a9a09f93e15ec4c6f5dd24d39
Ankit Lenka <ankitlenka26@gmail.com>
2021-05-07 19:02:12 +0530

    Iterators and Iterables

--- src/Generics/GenericList.java
+++ src/Generics/GenericList.java
@@ -1,6 +1,8 @@
 package Generics;
 
-public class GenericList <T> {
+import java.util.Iterator;
+
+public class GenericList <T> implements Iterable<T>{
     private T[] items = (T[])new Object[10];
     private int count;
     public void add(T item){
@@ -9,5 +11,27 @@ public class GenericList <T> {
     }
     public T get(int index){
         return items[index];
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new ListIterator(this);
+    }
+
+    // If in future we change the implementation of the list this class is the only class where we have to make any changes
+    private class ListIterator implements Iterator<T>{
+        private GenericList<T> list;
+        private int index;
+        public ListIterator(GenericList<T> list){
+            this.list = list;
+        }
+        @Override
+        public boolean hasNext() {
+            return (index < list.count);
+        }
+        @Override
+        public T next() {
+            return list.items[index++];
+        }
     }
 }
--- src/com/exceptions/Main.java
+++ src/com/exceptions/Main.java
@@ -11,11 +11,11 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) {
-        var max = Utils.max(1,2);
-        System.out.println(max);
-        var max1 = Utils.max(new User(10) , new User(20));
-        System.out.println(max1); // it will give the hashcode of the object
-
-        Utils.print("Ankit" , 20);
+        var list = new GenericList<Integer>();
+        list.add(1);
+        list.add(2);
+        for(var item : list){
+            System.out.println(item);
+        }
     }
 }
